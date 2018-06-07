30b8ecb9878e80fb5db6d3d5d7489708078773bd
wqr_xiaomi_pro <wqr571@outlook.com>
2018-06-07 10:25:39 +0800

    add the function

--- Settings.py
+++ Settings.py
@@ -10,7 +10,7 @@ class Settings():
         # 飞船的设置
         self.ship_speed_factor = 1.5
         self.ship_limit = 3
-        
+
         # 子弹设置
         self.bullet_speed_factor = 1
         self.bullet_width = 300
@@ -19,7 +19,7 @@ class Settings():
         self.bullets_allowed = 3
 
         # 外星人设置
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 10
         self.fleet_drop_speed = 10
         # fleet_direction为1表示向右移，为-1表示向左移
         self.fleet_direction = 1
